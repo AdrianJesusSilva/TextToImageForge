6ce4cde586f9454d2805c4555a5197f4527e0bc3
analetty <14-10515@usb.ve>
2023-11-22 13:50:53 -0400

    Corrige nombre clase TextAlign

---
+++ src/main/java/com/github/adrianjesussilva/textimageforge/enumerator/TextAlign.java
@@ -0,0 +1,17 @@
+package com.github.adrianjesussilva.textimageforge.enumerator;
+
+/**
+ * Enumerator that list the possibles aligns for the text in the image, relative to the margin
+ * 
+ * @author Adrian Jesus Simoes Silva 
+ * @author Ana Leticia Ibarra
+ *
+ */
+
+public enum TextAlign {
+
+	LEFT,
+	CENTER,
+	RIGHT;
+
+}
