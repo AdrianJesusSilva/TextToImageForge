7cc5ec2450095aee658261024698e4a3b170a281
Gustavo Quintana <gquintana@megasoft.com.ve>
2024-04-04 20:52:04 -0400

    Fix: Signature image resize
    Signatures with a width less than voucher were upscaled to match voucher width.
    
    Instead, signatures are merged as originally received.

--- src/test/java/com/github/adrianjesussilva/textimageforge/TestImageForge.java
+++ src/test/java/com/github/adrianjesussilva/textimageforge/TestImageForge.java
@@ -21,6 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.xml.bind.DatatypeConverter;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -205,6 +206,49 @@ log.info("Starting Dynamic Size Image");
 
 		
 		log.info("Ending Dynamic Size Image");
+	}
+	
+	@Test
+	@DisplayName("Test 04 Dynamic Size Image with Signature")
+	void test04DynamicSizeImage () {
+		
+		int voucherHeight = 70;
+		int voucherWidth = 120;
+		
+		//Vertical image 20 x 90
+		String signature = "iVBORw0KGgoAAAANSUhEUgAAABQAAABaCAIAAAA3ueFGAAAAAXNSR0IArs4c6QAAAANzQklUCAgI2+FP4AAAAvxJREFUWIXtl7tS3DAUhn+Z9GiTmsFUSRNmlTegzZICymQnyVDzCJ5J8gA8AlTUa2YYSqioodra+wKJvH3ik0JrcSzrYjZDKv5qJevzOT4XSQsarKIowFQURYZ/0DP8DD/D/xkmovVhIYT9PZ/P+eRoNHqE2/f399YdIYRSCsndp2kaItJaO440TZO2bABj1oZAKSWEGOr2zc0NH+7u7mLtVG1tba0PG6XhSKqHBmw+n/NQG71IwgDKsizLks+MRqOVV3FprTc3N7kjUkqtNRGl4ePjY8eR2WxmHqXhPM85eXBwYB8l4KqqHLPGYaNgtInIeMgnlVJSSjsMwiYxWmueoclkwtc8cZFE9JRwvyQfAUc+OAYbbLlcWvt9LxKpury8tC8iolU/cAshORUCoKoqviAGHx4ecpJXdQLuV3VZlh7Y7MyOTk5OOJnneX9N5g0jgNvbW/5oOp16ohpy23SPhe/u7vpr/LA9loyklN5l/jxfX1/z4d7enneZCxMRgLquBTLRPn3z9vUf/E7D8U5w5N+3hRCEJgn7v5mizZSAByqjtmPWgUO92rWQZT4f/W7bi0/Ccn+qLMuLiws+81K+El4zTsU5ZyIAeyama9vpRACz2czbsx5YKcXJ/u4RhPu7x2KxiMCZ/XJ0z0QhxHg83t7eTkfb5PlX/RNtvgk0+fA+QuIJzypqa5YCxduBnSK1w1DxZp23NgLtSECI8KVjBYfeGnK1A/PB1dUVH7rHmteCrRDHC1shofJ8sGzudXbIKyQYMPvL/Iuw2t/fT/js5lngocJEk9xAPcmIbElOCjywWdGIpulZdt6buNDE1YUJov3oDWzYHTNR23Vdn5+fh9YlUnV2drZYLPiD4I7pwNT+VbTa2dk5OjqKkyvY7URg+vETv5TH4LWVgUXLiABkg474DMDp6akTrSE+P1jmU3meD4kWAFRV5dgpiiKy0Xf6+ce378uu5YE+A50uXPkcOhP7wtfPXywspfReE4PwUtfvxgqAUsq5iyf1FytwXKrys4/uAAAAAElFTkSuQmCC";		
+		
+		int signatureOriginalHeight = 90;
+		int signatureTopPadding = 40;
+		
+		ImageForge iForge = ImageForge.builder()
+				.height(voucherHeight)
+				.width(voucherWidth)
+				.superiorMargin(15)
+				.leftMargin(15)
+				.inferiorMargin(15)
+				.rightMargin(15)
+				.lineSpacing(2)
+				.build();
+		
+		assertDoesNotThrow(() -> iForge.addLine("   TEST 04"), "It should not produce a exception");
+		
+		BufferedImage image = null;
+		try {
+			image = iForge.forgeImage(ImageEncoder.jpg, signature);
+		} catch (IOException | InvalidTextForgeConfigException e) {
+			fail ("it should not produce a exception");
+		}
+		assertNotNull(image);
+		
+		//Signature should not be resized if signature width is less than voucher width 
+		Assertions.assertTrue(image.getHeight() <= voucherHeight + signatureOriginalHeight + signatureTopPadding, "The resulting image has height greater than expected.");
+		
+		//Width should not change
+		Assertions.assertEquals(voucherWidth, image.getWidth());
+		
+		log.info("Ending Dynamic Size Image");
+		
 	}
 
 }
